1087e7b809e9f12542f43d88efa2f423aa38e14d
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2024-05-13 13:54:17 +0200

    move function head added

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ console.log(StartingText);
 
 StartingText.addEventListener("click", () => {
     StartingText.style.display = "none";
+    StartMovement();
 });
+
+function StartMovement() {
+    console.log("Move function active");
+}
