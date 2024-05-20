2cc2049b221ad3dd41c84aebfa880e9619fa5f64
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2024-05-20 09:04:42 +0000

    Obstacle generation

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay)); //sleep function
+
+// imports
+
 import Car from "./car.js";
+import Obstacle from "./obs.js";
 
+/* Global value declarations */
 let StartingText = document.querySelector("#startText");
-
 let HTMLLines = document.querySelectorAll(".col");
-
 let PCar = new Car("img/auto2.png-3"); // player's car
-
-let MaxLanes = 5; // amount of lanes in game
+const MaxLanes = 5; // amount of lanes in game
+let ObsList = []; // list storing obstacles
+let ObsListHTML = [] // obstacle HTML representations
+let GameOver = false;
 
 StartingText.addEventListener("click", () => {
     StartingText.style.display = "none";
     StartGame();
+});
+
+async function ObstacleGen() {
+    while (!GameOver) { // loop for making obstacles
+        GenerateObstacle();
+        await sleep(1000);
+    }
+}
+
+async function ObstacleMove() {
+    while (!GameOver) {
+        MoveObstacles(10);
+        await sleep(200);
+    }
+}
+
+function StartGame() {
+    DisplayCar(PCar.CurrLane);
     document.addEventListener("keydown", () => {
         console.log(event);
         if (event.keyCode == 39) {
-            console.log("right move");
-            if (!(PCar.CurrLane + 1 > 5)) {
+            if (!(PCar.CurrLane + 1 > MaxLanes)) {
                 PCar.CurrLane += 1;
-                console.log("move right");
                 DisplayCar(PCar.CurrLane);
             }
         }else if (event.keyCode == 37) {
@@ -27,10 +49,9 @@ StartingText.addEventListener("click", () => {
             }
         }
     });
-});
+    ObstacleGen();
+    ObstacleMove();
 
-function StartGame() {
-    DisplayCar(PCar.CurrLane);
 }
 
 function DisplayCar(Lane) {
@@ -39,11 +60,49 @@ function DisplayCar(Lane) {
             let NewImg = document.createElement("img");
             NewImg.src = PCar.imgSource;
             NewImg.classList.add("Car");
-            console.log(NewImg);
+            NewImg.id = "CAR";
             Column.appendChild(NewImg);
         }
         else{
-            Column.innerHTML = "";
+            let targetImg = Column.querySelector(".Car");
+            if (targetImg === null) {
+                // no break, so do nothing
+            }
+            else{
+                Column.removeChild(targetImg);
+            }
         }
     });
+}
+
+function GenerateObstacle() {
+    let RandomRow = (Math.floor(Math.random() * MaxLanes)) + 1;
+    let NewObs = new Obstacle(`/img/akadaly.png-${RandomRow}-632-568`);
+    ObsList.push(NewObs);
+    let NewObsHTML = document.createElement("img");
+
+    HTMLLines.forEach(Column => {
+        if (Column.id == `C-${RandomRow}`) {
+            NewObsHTML.src = NewObs.imgSource;
+            NewObsHTML.classList.add("obs");
+            NewObsHTML.style.position = "absolute";
+//            NewObsHTML.classList.add("obsT1");
+            NewObsHTML.style.bottom = `${NewObs.YLowPos}px`;
+            NewObsHTML.id = `${Column.id}-`
+            Column.appendChild(NewObsHTML);
+            ObsListHTML.push(NewObsHTML);
+        }
+    });
+    console.log("OBS generated")
+}
+
+
+
+async function MoveObstacles(amount) {
+    for (let index = 0; index < ObsList.length; index++) {
+        console.log("MOVE ACTIVE");
+        ObsList[index].YLowPos -= amount;
+        ObsList[index].YHighPos -= amount;
+        ObsListHTML[index].style.bottom -= amount;
+    }
 }
