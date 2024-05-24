ee0a78f73dc564518f68c2bb727bf7652fa7e92c
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2024-05-24 17:10:43 +0000

    Outdated comment removed

--- car.js
+++ car.js
@@ -4,6 +4,6 @@ export default class Car{
         this.imgSource = Datas[0];
         this.CurrLane = Number(Datas[1]);
         this.YLowPos = 10;
-        this.YHighPos = 180; // car size is +170, reduced to +160 to counter lag
+        this.YHighPos = 180;
     }
 }
