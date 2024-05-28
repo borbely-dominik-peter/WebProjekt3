a83e49b9639cae75113fbff15569694f3c6dcb99
hajba-balint <hajba.balint@students.jedlik.eu>
2024-05-28 18:03:21 +0200

    Testing

--- GameTest/GameTest/UnitTest1.cs
+++ GameTest/GameTest/UnitTest1.cs
@@ -26,8 +26,9 @@ namespace GameTest
             Assert.Equal("Start Game!", driver.FindElement(By.XPath("/html/body/div[2]/h1[1]")).Text);
             Assert.Equal("Score:", driver.FindElement(By.XPath("/html/body/div[1]/p[1]")).Text);
             Assert.Equal(5, driver.FindElements(By.XPath("/html/body/div[2]/table/tbody/tr/td")).Count);
-            // Assert.Equal($"url({})", driver.FindElement(By.XPath("/html/body/div[2]/table/tbody/tr/td[1]")).GetCssValue("background-image"));
-            // Assert.Equal($"url({})", driver.FindElement(By.XPath("/html/body/div[2]/table/tbody/tr/td[5]")).GetCssValue("background-image"));
+            // Assert.Equal($"url()", driver.FindElement(By.XPath("/html/body/div[2]/table/tbody/tr/td[1]")).GetCssValue("background-image"));
+            // Assert.Equal($"url()", driver.FindElement(By.XPath("/html/body/div[2]/table/tbody/tr/td[5]")).GetCssValue("background-image"));
         }
+
     }
 }
